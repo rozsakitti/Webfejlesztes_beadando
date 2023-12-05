834abe12fa19bb86ea1e046c4f33d1939addb0ad
rozsakitti <rozsa.kitty56@gmail.com>
2023-12-05 18:07:45 +0100

    first version of project

---
+++ src/main/java/com/example/WebfejlesztesBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfejlesztesBeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfejlesztesBeadandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webfejlesztes_beadando/WebfejlesztesBeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfejlesztes_beadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfejlesztesBeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
